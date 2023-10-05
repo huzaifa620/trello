396da1a84f3b12edbb19fec3477b39bccb960747
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-10-05 05:04:19 +0500

    Header done

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Image from "next/image"
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid"
+import Avatar from "react-avatar"
+
+const Header = () => {
+  return (
+    <header>
+
+        <div className="flex flex-col md:flex-row items-center justify-center p-5 bg-gray-500/10 rounded-b-2xl">
+
+            <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055d1] rounded-md filter blur-3xl opacity-50 -z-50" />
+
+            <Image src="https://links.papareact.com/c2cdd5" alt="" width={300} height={100} className="w-44 md:w-56 md:pb-0 object-contain cursor-pointer hover:animate-pulse" />
+
+            <div className="flex items-center space-x-5 flex-1 justify-end max-w-7xl">
+                <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
+                    <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+                    <input type="text" placeholder="Search" className="flex-1 outline-none p-2" />
+                    <button hidden type="submit">
+                        Search
+                    </button>
+                </form>
+
+                <Avatar className="cursor-pointer hover:opacity-90" name="M Huzaifa" round color="#0055D1" size="50" />
+            </div>
+        </div>
+
+        <div className="flex items-center justify-center py-2 px-5 md:py-5">
+            <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+                <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+                GPT is summarizing your tasks for the day...
+            </p>
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
